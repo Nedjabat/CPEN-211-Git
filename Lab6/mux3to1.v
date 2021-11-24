126fa3be1fa7889c472a1443ee8ec37e21478fbe
Nedjabat <56488093+Nedjabat@users.noreply.github.com>
2021-11-24 09:11:24 -0800

    fix multiplexer

--- Lab6/mux3to1.v
+++ Lab6/mux3to1.v
@@ -2,6 +2,13 @@ module mux3to1 (a,b,c,sel,out);
     parameter n=16;
     input [n-1: 0] a, b, c;
     input [1:0] sel;
-    output [n-1: 0] out;
-    assign out = sel[1] ? (sel[0] ? 0 : c) : (sel[0] ? b : a);
+    output reg [n-1: 0] out;
+    always @(*) begin
+        casex({sel})
+        2'b00 : out = a;
+        2'b01 : out = b;
+        2'b10 : out = c;
+        2'bxx : out = 0;
+    end
+    //assign out = sel[1] ? (sel[0] ? 0 : c) : (sel[0] ? b : a);
 endmodule
