09c40fdbfc6da2731d39a12e08e7d1ae8f0c13ca
Nedjabat <ryannedjabat17@gmail.com>
2021-11-29 18:20:46 -0800

    changes xxx

--- Lab7/CPU.v
+++ Lab7/CPU.v
@@ -4,10 +4,12 @@ module cpu(clk,reset,s,load,in,out,N,V,Z,w);
     output [15:0] out;
     output N, V, Z, w;
     reg [15:0]  in_out;
+    reg [8:0] pc_out, data_out;
     wire [15:0] in, next_out_in,sximm5,sximm8;
+    wire [8:0] next_pc, incremental, next_out_pc, next_out_data; 
     wire [2:0] opcode,readnum,writenum;
     wire [1:0] op, vsel, nsel, ALUop, shift;
-    wire load, loads, reset, w, s, clk, loada, loadb, loadc, asel, bsel, write;
+    wire load, loads, reset, w, s, clk, loada, loadb, loadc, asel, bsel, write, load_pc, reset_pc, addr_sel, load_addr;
 
     assign next_out_in = load ? in : in_out;  
 
@@ -19,23 +21,32 @@ module cpu(clk,reset,s,load,in,out,N,V,Z,w);
     end
 
     decoder dec(in_out,nsel,ALUop,sximm5,sximm8,shift,readnum,writenum,opcode,op);
-    FSM machine(reset,w,loads,s,op, opcode,clk,loada,loadb,loadc,asel,bsel,vsel,write,nsel);
-    datapath DP(clk,N,V,Z,write,vsel,loada,loadb,asel,bsel,loadc,loads,readnum,writenum,shift,ALUop,out,sximm5,sximm8);
+    FSM machine(reset,w,loads,s,op, opcode,clk,loada,loadb,loadc,asel,bsel,vsel,write,nsel,load_addr);
+    datapath DP(clk,N,V,Z,write,vsel,loada,loadb,asel,bsel,loadc,loads,readnum,writenum,shift,ALUop,out,sximm5,sximm8,in);
     
     assign next_pc = reset_pc ?  incremental : 9'b0;
 
     assign next_out_pc = load_pc ? next_pc : pc_out;
 
-    assign incremental = next_out_pc  + 9'b000000001;
+    assign incremental = next_pc  + 9'b000000001;
 
 
-    assign mem_addr = addr_sel ? 9'b0 : next_out_pc;
+    assign mem_addr = addr_sel ? data_out : next_pc;
 
 
     always @(posedge clk)
     begin
 	
 	    pc_out = next_out_pc; 
+	
+    end
+    
+    assign next_out_data = load_addr ? out[8:0] : data_out;
+    
+    always @(posedge clk)
+    begin
+	
+	    data_out = next_out_data; 
 	
     end
 endmodule
--- Lab7/lab7_top.v
+++ Lab7/lab7_top.v
@@ -1,15 +1,28 @@
-module lab7_top(KEY,SW,LEDR,HEX0,HEX1,HEX2,HEX3,HEX4,HEX5);
+module lab7_top(KEY,SW,LEDR,HEX0,HEX1,HEX2,HEX3,HEX4,HEX5,mem_cmd,mem_addr,read_data, write_data);
 input [3:0] KEY;
 input [9:0] SW;
+input [1:0] mem_cmd;
+input [8:0] mem_addr;
+input [15:0] write_data;
 output [9:0] LEDR;
 output [6:0] HEX0, HEX1, HEX2, HEX3, HEX4, HEX5;
-RAM (#) MEM(KEY[0])
-assign dout = enable ? read_data : {16{1’bz}};
-assign inand1 = `MREAD && mem_cmd;
-assign inand2 = 1'b0 && mem_addr;
-assign enable = inand1 && inand2; 
-        
+output [15:0] read_data;
+wire enable;
+wire [1:0] inand1, inand2, inand3;
+`define MNONE   4'b00
+`define MREAD   4'b01
+`define MWRITE  4'b10
 
+assign out = enable ? read_data : 16'bz;
+assign inand1 = `MREAD & mem_cmd;
+assign inand2 = 1'b0 & mem_addr;
+assign inand3 = `MWRITE & mem_cmd;
+assign write = inand2 & inand3;
+assign enable = inand1 & inand2; 
+
+RAM #(16) MEM(KEY[0],mem_addr[7:0],mem_addr[7:0],write,write_data ,out);
+
+endmodule        
 
 module RAM(clk,read_address,write_address,write,din,dout);
   parameter data_width = 32; 
