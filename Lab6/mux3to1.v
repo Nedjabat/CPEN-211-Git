e822937251e0877868e0fd42f2bc015cb669fa4c
Nedjabat <56488093+Nedjabat@users.noreply.github.com>
2021-12-01 17:36:12 -0800

    change

--- Lab6/mux3to1.v
+++ Lab6/mux3to1.v
@@ -3,7 +3,7 @@ module mux3to1 (a,b,c,sel,out);
     input [n-1: 0] a, b, c;
     input [1:0] sel;
     output reg [n-1: 0] out;
-    always @(*) begin
+    /*always @(*) begin
         casex({sel})
         2'b00 : out = a;
         2'b01 : out = b;
@@ -11,5 +11,6 @@ module mux3to1 (a,b,c,sel,out);
         2'bxx : out = 0;
         endcase
     end
+    */
     //assign out = sel[1] ? (sel[0] ? 0 : c) : (sel[0] ? b : a);
 endmodule
--- lpt3project1/q1.v
+++ lpt3project1/q1.v
@@ -10,7 +10,7 @@ module datapath(clk,in,sr,Rn,w,aluop,lt,tsel,bsel,out);
     always @(*) begin     //next state case statement
         casex({Rn, w})
             {2'b00,1'b1} : {load0,load1,load2,load3} = 4'b1000;
-            {2'b01,1'b1} : {load0,load1,load2,load3} = 4'b0100;
+            {2'b01,1'b1} : {load0,load1,load2,load3} = 4'b0100; 
             {2'b10,1'b1} : {load0,load1,load2,load3} = 4'b0010;
             {2'b11,1'b1} : {load0,load1,load2,load3} = 4'b0001;
             default: {load0,load1,load2,load3} = 4'bxxxx;
@@ -39,5 +39,40 @@ module datapath(clk,in,sr,Rn,w,aluop,lt,tsel,bsel,out);
     always @(posedge clk) begin
         R3 = next_R3;
     end
+    assign next_lt = lt ? rin : temp;
 
+    always @(posedge clk) begin
+        temp = next_lt;
+    end
+    always @(*) begin
+        casex(ALUop)
+            2'b00: alu_out = temp ^ Bin; 
+            2'b01: alu_out = temp & Bin;
+            2'b10: alu_out = temp << 1;
+            2'b11: alu_out = Bin;
+            default: alu_out =  temp;
+        endcase
+    end
+    assign out = R0;
+    assign Bin = bsel [0] ? (bsel[1] ? (bsel[2] ? 0 : R3):R2 ) : R1;
+    assign itin = tsel [0] ? (tsel[1] ? (tsel[2] ? 0 : Bin):R0 ) : alu_out;
+    mux3to1 #(8) (in,alu_out,tmp,sr,rin)
+endmodule
+
+module mux3to1 (a,b,c,sel,out);
+    parameter n=16;
+    input [n-1: 0] a, b, c;
+    input [1:0] sel;
+    output reg [n-1: 0] out;
+    /*always @(*) begin
+        casex({sel})
+        2'b00 : out = a;
+        2'b01 : out = b;
+        2'b10 : out = c;
+        2'bxx : out = 0;
+        endcase
+    end
+    */
+    assign out = sel[1] ? (sel[0] ? 0 : c) : (sel[0] ? b : a);
+endmodule
     
